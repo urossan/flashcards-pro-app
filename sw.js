a072ac24dcb24bacc92d34595d2794e43ac3e206
urossan <30761385+urossan@users.noreply.github.com>
2025-06-10 09:11:57 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'flashcards-pro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/lucide-react@latest/dist/umd/lucide-react.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
